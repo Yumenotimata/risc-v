d82c511b2ccabd983757975d543ad4e85688a39d
Yumenotimata <kouyayamaoka@gmail.com>
2023-08-08 20:03:28 +0900

    core

--- multicycle-type/memory.v
+++ multicycle-type/memory.v
@@ -1,8 +1,8 @@
 module Memory(
     input clk,
-    input wire [31:0] memory_read_addr,memory_write_addr,
+    input wire [31:0] memory_out_addr,memory_write_addr,
     input wire [31:0] memory_write_data,
-    output wire[31:0] memory_read_data
+    output wire[31:0] memory_out_data
 );
 
     reg [7:0] m[0:65535];
@@ -11,6 +11,10 @@ module Memory(
         m[memory_write_addr] <= memory_write_data;
     end
 
-    assign memory_read_data = m[memory_read_addr];
+    assign memory_out_data = m[memory_out_addr];
+
+    initial begin
+        $readmemb("memory.hex",core.memory.m,16'h0,16'hffff);
+    end
 
 endmodule
